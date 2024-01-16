0d978977d868193d1b8fccb3e742485501575747
Mrbysco <Mrbysco@users.noreply.github.com>
2024-01-16 23:28:48 +0100

    Setup multiloader

---
+++ forge/src/main/java/com/mrbysco/itemframent/ItemFramentForge.java
@@ -0,0 +1,11 @@
+package com.mrbysco.itemframent;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class ItemFramentForge {
+
+    public ItemFramentForge() {
+
+    }
+}
