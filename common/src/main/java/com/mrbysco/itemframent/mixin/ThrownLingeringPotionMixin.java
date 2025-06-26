e512cbd1db71d5b695d571f54bf5d9fa61cc538d
Mrbysco <Mrbysco@users.noreply.github.com>
2025-06-26 21:29:58 +0200

    Port to 1.21.6

--- common/src/main/java/com/mrbysco/itemframent/mixin/ThrownLingeringPotionMixin.java
+++ common/src/main/java/com/mrbysco/itemframent/mixin/ThrownLingeringPotionMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.projectile.ThrownLingeringPotion;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.alchemy.PotionContents;
+import net.minecraft.world.phys.HitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,8 +16,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ThrownLingeringPotion.class)
 public class ThrownLingeringPotionMixin {
-	@Inject(method = "onHitAsPotion(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/Entity;)V", at = @At("HEAD"))
-	public void itemframent$applySplash(ServerLevel serverLevel, ItemStack stack, Entity entity, CallbackInfo ci) {
+	@Inject(method = "onHitAsPotion(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/phys/HitResult;)V", at = @At("HEAD"))
+	public void itemframent$applySplash(ServerLevel serverLevel, ItemStack stack, HitResult result, CallbackInfo ci) {
 		ThrownLingeringPotion potion = (ThrownLingeringPotion) (Object) this;
 		PotionContents potioncontents = stack.getOrDefault(DataComponents.POTION_CONTENTS, PotionContents.EMPTY);
 		Iterable<MobEffectInstance> effects = potioncontents.getAllEffects();
--- common/src/main/java/com/mrbysco/itemframent/mixin/ThrownSplashPotionMixin.java
+++ common/src/main/java/com/mrbysco/itemframent/mixin/ThrownSplashPotionMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.projectile.ThrownSplashPotion;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.HitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,12 +15,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ThrownSplashPotion.class)
 public class ThrownSplashPotionMixin {
-	@Inject(method = "onHitAsPotion(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/Entity;)V", at = @At(
+	@Inject(method = "onHitAsPotion(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/phys/HitResult;)V", at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/world/phys/AABB;inflate(DDD)Lnet/minecraft/world/phys/AABB;",
 			shift = At.Shift.AFTER,
 			ordinal = 0))
-	public void itemframent$applySplash(ServerLevel serverLevel, ItemStack stack, Entity entity, CallbackInfo ci, @Local Iterable<MobEffectInstance> effects) {
+	public void itemframent$applySplash(ServerLevel serverLevel, ItemStack stack, HitResult result, CallbackInfo ci, @Local Iterable<MobEffectInstance> effects) {
 		ThrownSplashPotion potion = (ThrownSplashPotion) (Object) this;
 		CommonClass.handleSplash(effects, potion);
 	}
